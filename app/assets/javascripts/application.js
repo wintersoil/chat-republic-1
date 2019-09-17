4de481408c9b9fe725e0f3285ef853874cd9cf1e
wintersoil <ali.usmani.182@gmail.com>
2019-09-18 00:34:03 +0400

    Fixed something

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,14 +28,14 @@ scroll_bottom = function() {
 $(document).ready(function(){
   scroll_bottom();
 
-  $('#message_body').on("keypress", function(e){
+  $('#message_body').keypress(function(e){
     if(e.keyCode == 13)
     {
       $('.messages-input-field').val("");
     }
   });
-  $('.message-input-button').on("click", function(e){
+  $('.message-input-button').click(function(e){
     $('.messages-input-field').val("");
   });
 
-})
+});

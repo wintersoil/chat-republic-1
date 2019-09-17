6eb2ee1037336d919b92568a794e0c842d54c6ab
wintersoil <ali.usmani.182@gmail.com>
2019-09-17 06:05:25 +0400

    Added channel also

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
+    identified_by :current_user    # creates a instance variable
+
+        def connect
+          session_from_cookie = cookies.encrypted[Rails.application.config.session_options[:key]]
+          user_id = session_from_cookie['user_id']
+          reject_unauthorized_connection if user_id.nil?
+          self.current_user = User.find(user_id)
+          Rails.logger.warn("connection for user #{current_user}")
+          reject_unauthorized_connection if current_user.nil?
+        end
+
+        def disconnect
+          # Any cleanup work needed when the cable connection is cut.
+        end
+
   end
 end

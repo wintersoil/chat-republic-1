047b1714cb5c4a344df52b11b2736a2e312a2ab2
wintersoil <ali.usmani.182@gmail.com>
2019-09-17 05:55:50 +0400

    Added messages model and user password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_014557) do
+ActiveRecord::Schema.define(version: 2019_09_17_015444) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_014557) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

ffc648491fa68b6e9c87df4eb5054c8037a2bb64
wintersoil <ali.usmani.182@gmail.com>
2019-09-17 07:22:53 +0400

    Redis changed

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,47 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+scroll_bottom = function() {
+  if($('.messages-display').length > 0) {
+    $('.messages-display').scrollTop($('.messages-display')[0].scrollHeight);
+  }
+}
+
+$(document).ready(function(){
+  scroll_bottom();
+})

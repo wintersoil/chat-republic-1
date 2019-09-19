5b28e2216c8b7ad20f435e1f69bfa4aea221481b
wintersoil <ali.usmani.182@gmail.com>
2019-09-19 05:59:32 +0400

    Password editing capability update put route

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,11 +5,17 @@ class ProfileController < ApplicationController
   end
 
   def update
-    if @user.save
+    if @user.update(user_params)
       redirect_to profile_path(@user)
     else
       render "new"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password)
   end
 
 end

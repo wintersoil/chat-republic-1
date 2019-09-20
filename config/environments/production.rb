a2834f4c4eac6eda147fec823f3e6d6c8b90f5bc
wintersoil <ali.usmani.182@gmail.com>
2019-09-20 13:14:34 +0400

    Mp3 capability

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.web_socket_server_url = "wss://DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
-  config.action_cable.url = "http://DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
+  config.action_cable.url = "wss://DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
   config.action_cable.disable_request_forgery_protection = true
   config.force_ssl = false
 

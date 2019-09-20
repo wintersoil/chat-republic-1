af39cd23cf7b634d049673965580810e69aad52c
wintersoil <ali.usmani.182@gmail.com>
2019-09-21 03:03:18 +0400

    Added mp3 capability

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -26,6 +26,8 @@ class ChatroomController < ApplicationController
       @message.mp3.file.delete
     else
     end
+    Fog::Storage::AWS.send(:remove_const, 'DEFAULT_REGION')
+    Fog::Storage::AWS::DEFAULT_REGION = 'ca-central-1'
     s3 = Fog::AWS::Storage.new(:aws_access_key_id => ENV['S3_ACCESS_KEY'], :aws_secret_access_key => ENV['S3_SECRET_KEY'], :region => "ca-central-1")
     directory = s3.directories.get("uploads")
     name = ('a'..'z').to_a.shuffle[0..7].join + ".#{extension}"

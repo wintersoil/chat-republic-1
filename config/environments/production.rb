e0ae97b52ca9637727c6c9075552ad56f96f44fa
wintersoil <ali.usmani.182@gmail.com>
2019-09-20 13:11:23 +0400

    Mp3 capability

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.web_socket_server_url = "DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
+  config.web_socket_server_url = "wss://DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
   config.action_cable.url = "http://DemoApplication1-env.hw7neqyjpm.ca-central-1.elasticbeanstalk.com/cable"
   config.action_cable.disable_request_forgery_protection = true
   config.force_ssl = false

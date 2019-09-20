ee0223c8e423428741fbfa3357e5ae87b7c3f997
wintersoil <ali.usmani.182@gmail.com>
2019-09-20 08:55:34 +0400

    Mobile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.force_ssl = true
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = '/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]

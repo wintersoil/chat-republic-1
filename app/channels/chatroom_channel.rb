1642b90eb0defd61f1dc56850d2e1120475892d6
wintersoil <ali.usmani.182@gmail.com>
2019-09-21 14:13:56 +0400

    User online

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -2,16 +2,14 @@ class ChatroomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     current_user.is_online = 1
+    current_user.save
     stream_from "chatroom_channel"
   end
 
   def unsubscribed
     current_user.is_online = 0
+    current_user.save
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def current_user
-    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
-  end
-  
 end

d739cb88c70e65d41c14541212b9a4d2b337b34a
wintersoil <ali.usmani.182@gmail.com>
2019-09-21 23:29:06 +0400

    User online

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -3,12 +3,14 @@ class ChatroomChannel < ApplicationCable::Channel
     # stream_from "some_channel"
     current_user.is_online = 1
     current_user.save
+    ActionCable.server.broadcast "chatroom_channel", online: {user_name: current_user.user_name, first_name: current_user.first_name, last_name: current_user.last_name}
     stream_from "chatroom_channel"
   end
 
   def unsubscribed
     current_user.is_online = 0
     current_user.save
+    ActionCable.server.broadcast "chatroom_channel", offline: {user_name: current_user.user_name, first_name: current_user.first_name, last_name: current_user.last_name}
     # Any cleanup needed when channel is unsubscribed
   end
 

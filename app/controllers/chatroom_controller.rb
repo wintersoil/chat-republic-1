54402870e43ddd4c745a20e0d1d8e0ce5eee29b5
wintersoil <ali.usmani.182@gmail.com>
2019-09-21 04:32:43 +0400

    Mobile

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -21,7 +21,8 @@ class ChatroomController < ApplicationController
     @message = Message.new(body: "mp3")
     @message.user = current_user
     @user = current_user
-    audio = request.body.read
+    audio = ActiveSupport::JSON.decode(request.body.read).data
+    puts audio
     if @message.mp3.present?
       @message.mp3.file.delete
     else

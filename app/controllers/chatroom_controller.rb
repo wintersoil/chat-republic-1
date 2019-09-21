05d8ebfaaac06702381f8e3ad91008d41e516841
wintersoil <ali.usmani.182@gmail.com>
2019-09-21 05:16:18 +0400

    Mobile

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -18,7 +18,7 @@ class ChatroomController < ApplicationController
   end
 
   def mp3audio
-    @message = Message.new(msg_params)
+    @message = Message.new
     @message.body = params[:body]
     @message.user = current_user
     @user = current_user

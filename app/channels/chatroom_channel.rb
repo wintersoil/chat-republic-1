73870d5086fa5e9097937750b6cb4707033258a6
wintersoil <ali.usmani.182@gmail.com>
2019-09-22 00:02:35 +0400

    User online

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,20 +1,20 @@
 class ChatroomChannel < ApplicationCable::Channel
 
-  arraying = []
+  @arraying = []
 
   def subscribed
     # stream_from "some_channel"
     current_user.is_online = 1
     current_user.save
-    arraying.push({user_name: current_user.user_name, first_name: current_user.first_name, last_name: current_user.last_name})
-    ActionCable.server.broadcast "chatroom_channel", online: online_render(arraying)
+    @arraying.push({user_name: current_user.user_name, first_name: current_user.first_name, last_name: current_user.last_name})
+    ActionCable.server.broadcast "chatroom_channel", online: online_render(@arraying)
     stream_from "chatroom_channel"
   end
 
   def unsubscribed
     current_user.is_online = 0
     current_user.save
-    result = arraying.select do |elem|
+    result = @arraying.select do |elem|
       elem.user_name != current_user.user_name
     end
     ActionCable.server.broadcast "chatroom_channel", online: online_render(result)

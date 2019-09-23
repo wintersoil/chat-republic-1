64280877c6e04145cb25378fc03435d2245af936
wintersoil <ali.usmani.182@gmail.com>
2019-09-23 19:43:46 +0400

    Added video channel and pages and controller

--- app/channels/video_channel.rb
+++ app/channels/video_channel.rb
@@ -1,10 +1,18 @@
 class VideoChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_for current_user
+    stream_for current_user.user_name
+
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def handle_messages(data)
+    @client = User.find(data.client)
+    @user = User.find(data.id)
+    VideoChannel.broadcast_to(@client.user_name, { image: data.image })
+    VideoChannel.broadcast_to(@user.user_name, { image: data.image })
   end
 end

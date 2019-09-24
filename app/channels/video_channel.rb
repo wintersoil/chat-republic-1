5428a50d06794bccd5825a28590c54fc5a168eac
wintersoil <ali.usmani.182@gmail.com>
2019-09-24 15:54:06 +0400

    Updating images in live page

--- app/channels/video_channel.rb
+++ app/channels/video_channel.rb
@@ -1,22 +1,16 @@
 class VideoChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "video_channel"
-
-    #stream_from "#{self.channel_name}:#{current_user.to_gid_param}"
-    #@videoClient = VideoClient.find_by(user: current_user)
-    #@client1 = User.find(@videoClient.client_id)
+    stream_from "#{self.channel_name}:#{current_user.to_gid_param}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    #stop_all_streams
+    stop_all_streams
   end
 
   def handle_messages(data)
-    ActionCable.server.broadcast("video_channel", data)
-    #@videoClient = VideoClient.find_by(user: current_user)
-    #@client1 = User.find(@videoClient.client_id)
-    #VideoChannel.broadcast("#{self.channel_name}:#{@client1.to_gid_param}", data: { image: data["image"] })
+    @client1 = User.find(data['receiver_id'])
+    ActionCable.server.broadcast("#{self.channel_name}:#{@client1.to_gid_param}", data)
   end
 end

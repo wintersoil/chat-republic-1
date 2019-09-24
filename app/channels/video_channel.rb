41b5103fc2fc326a0fed665bd31c4ce8fb347e99
wintersoil <ali.usmani.182@gmail.com>
2019-09-24 11:04:08 +0400

    Fixed recording issue

--- app/channels/video_channel.rb
+++ app/channels/video_channel.rb
@@ -1,18 +1,22 @@
 class VideoChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from current_user.user_name
+    stream_from "#{self.channel_name}:#{current_user.to_gid_param}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
+  def user
+    User.find_by(id: params[:id])
+  end
+
   def handle_messages(data)
     @client = User.find(data.data.client)
-    stream_from @client.user_name
+    stream_from "#{self.channel_name}:#{@client.to_gid_param}"
     @user = User.find(data.data.id)
-    VideoChannel.broadcast(current_user.user_name, data: { image: data.data.image })
-    VideoChannel.broadcast(@client.user_name, data: { image: data.data.image })
+    VideoChannel.broadcast("#{self.channel_name}:#{current_user.to_gid_param}", data: { image: data.data.image })
+    VideoChannel.broadcast("#{self.channel_name}:#{@client.to_gid_param}", data: { image: data.data.image })
   end
 end

961af55f38cf47bcfa361050acbd8a89f6771b67
wintersoil <ali.usmani.182@gmail.com>
2019-09-24 10:44:52 +0400

    Fixed recording issue

--- app/channels/video_channel.rb
+++ app/channels/video_channel.rb
@@ -2,7 +2,6 @@ class VideoChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stream_from current_user.user_name
-
   end
 
   def unsubscribed
@@ -11,8 +10,9 @@ class VideoChannel < ApplicationCable::Channel
 
   def handle_messages(data)
     @client = User.find(data.data.client)
+    stream_from @client.user_name
     @user = User.find(data.data.id)
-    VideoChannel.broadcast_to(@client, data: { image: data.data.image })
-    VideoChannel.broadcast_to(@user, data: { image: data.data.image })
+    VideoChannel.broadcast(current_user.user_name, data: { image: data.data.image })
+    VideoChannel.broadcast(@client.user_name, data: { image: data.data.image })
   end
 end

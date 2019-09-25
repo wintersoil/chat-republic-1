61f32817b8183d929a21c936c233b64cd7c7a254
wintersoil <ali.usmani.182@gmail.com>
2019-09-26 00:56:15 +0400

    Updating video record button

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,6 +2,14 @@ class ChatController < ApplicationController
 
   def index
     @users = User.all
+    all_online = @users.select do |elm|
+      elm.online?
+    end
+    only_relevant = []
+    all_online.each do |elm|
+      only_relevant.push({event: 'appear', user_id: elm.id})
+    end
+    ActionCable.server.broadcast "online_channel", {arrayez: only_relevant}
   end
 
 end

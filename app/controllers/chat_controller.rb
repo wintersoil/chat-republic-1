654d519f00ed75665f568da1d249d3a6d38bd8f5
wintersoil <ali.usmani.182@gmail.com>
2019-09-26 01:19:43 +0400

    Updating video record button

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,8 +1,14 @@
 class ChatController < ApplicationController
+after_action :broadcasting_online, only: :index
 
   def index
-    current_user.appear
     @users = User.all
+  end
+
+  private
+
+  def broadcasting_online
+    current_user.appear
   end
 
 end

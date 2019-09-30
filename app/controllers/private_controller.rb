e52a44eb040429590c4b0812ab48c70e37d4509b
wintersoil <ali.usmani.182@gmail.com>
2019-10-01 01:18:34 +0400

    Coffeescript

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -1,5 +1,5 @@
 class PrivateController < ApplicationController
-  before_action :sanitize_page_params
+  before_action :sanitize_page_params, only: [:create]
 
   def new
     @private_message = PrivateMessage.new

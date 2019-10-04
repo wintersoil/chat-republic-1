27ddec7ba338aab6999c0b678c9393d6c9583496
wintersoil <ali.usmani.182@gmail.com>
2019-10-04 13:47:25 +0400

    Private messages

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -38,10 +38,10 @@ class PrivateController < ApplicationController
     else
     end
     s3 = Fog::AWS::Storage.new(:aws_access_key_id => ENV['S3_ACCESS_KEY'], :aws_secret_access_key => ENV['S3_SECRET_KEY'], :region => "ca-central-1")
-    directory = s3.directories.get("aliphotoappimages/private_messages")
+    directory = s3.directories.get("aliphotoappimages")
 
     extension = 'mp3'
-    name = ('a'..'z').to_a.shuffle[0..7].join + ".#{extension}"
+    name = "/private_messages/" + ('a'..'z').to_a.shuffle[0..7].join + ".#{extension}"
     data = audio1
     file = directory.files.create(:key => name,:body => data,:public => true)
     file.save
@@ -71,7 +71,7 @@ class PrivateController < ApplicationController
     directory = s3.directories.get("aliphotoappimages/private_messages")
 
     extension = 'mp4'
-    name = ('a'..'z').to_a.shuffle[0..7].join + ".#{extension}"
+    name = "/private_messages/" + ('a'..'z').to_a.shuffle[0..7].join + ".#{extension}"
     data = audio1
     file = directory.files.create(:key => name,:body => data,:public => true)
     file.save

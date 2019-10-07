7311534ccef194d0a052de47f9e7691572d4b592
wintersoil <ali.usmani.182@gmail.com>
2019-10-07 23:56:44 +0400

    Video Overlay

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -23,11 +23,13 @@ class LoginController < ApplicationController
   end
 
   def notify_online_controller_action
-    action = params[:action]
-    controller = params[:controller]
-    only_relevant = []
-    only_relevant.push({event: 'appear', user_id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, controller: controller, action: action})
-    ActionCable.server.broadcast "online_channel", {arrayez: only_relevant}
+    if(logged_in?)
+      action = params[:action]
+      controller = params[:controller]
+      only_relevant = []
+      only_relevant.push({event: 'appear', user_id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, controller: controller, action: action})
+      ActionCable.server.broadcast "online_channel", {arrayez: only_relevant}
+    end
   end
 
 end

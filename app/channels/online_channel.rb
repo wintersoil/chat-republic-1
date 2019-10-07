a52d79b04f0f9a05ee195f04556631d9d0328cc6
wintersoil <ali.usmani.182@gmail.com>
2019-10-07 20:28:41 +0400

    Video Overlay

--- app/channels/online_channel.rb
+++ app/channels/online_channel.rb
@@ -1,16 +1,16 @@
 class OnlineChannel < ApplicationCable::Channel
   def subscribed
     stream_from "online_channel"
-    controller = ApplicationController.request.params[:controller]
-    action = ApplicationController.request.params[:action]
+    controller = ApplicationController.params[:controller]
+    action = ApplicationController.params[:action]
     current_user.appear(controller,action)
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     stream_from "online_channel"
-    controller = ApplicationController.request.params[:controller]
-    action = ApplicationController.request.params[:action]
+    controller = ApplicationController.params[:controller]
+    action = ApplicationController.params[:action]
     current_user.disappear(controller,action)
   end
 end

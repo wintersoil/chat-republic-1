74c153ea184d91dd51d2bcad9cc4218de718b6d7
wintersoil <ali.usmani.182@gmail.com>
2019-10-12 16:40:27 +0400

    Fixed chatting

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,4 @@
 class LoginController < ApplicationController
-  before_action :notify_online_controller_action
 
   def new
   end
@@ -20,16 +19,6 @@ class LoginController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have been successfully logged out."
     redirect_to root_path
-  end
-
-  def notify_online_controller_action
-    if(logged_in?)
-      action = params[:action]
-      controller = params[:controller]
-      only_relevant = []
-      only_relevant.push({event: 'appear', user_id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, controller: controller, action: action})
-      ActionCable.server.broadcast "online_channel", {arrayez: only_relevant}
-    end
   end
 
 end

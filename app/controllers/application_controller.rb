c58c795643d7b20e3c9c69a7d416fcc7f816ed8b
wintersoil <ali.usmani.182@gmail.com>
2019-10-14 17:44:51 +0400

    Use redis for cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
     super
     $cache = Redis.new(host:"localhost")
     empty_arraying = []
-    $cache.hmset('current_on_chatroom', empty_arraying.to_json)
+    $cache.hset('current_on_chatroom', empty_arraying.to_json)
   end
 
   def remove_from_chatroom

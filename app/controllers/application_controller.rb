bfdfb1fee65c09544e161f9be8830ae2855c42e5
wintersoil <ali.usmani.182@gmail.com>
2019-10-14 18:15:03 +0400

    Use redis for cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,23 +33,27 @@ class ApplicationController < ActionController::Base
 
   def remove_from_chatroom
     current_on_chatroom = JSON.parse($cache.get('current_on_chatroom'))
-    if logged_in? && current_on_chatroom.include?(current_user)
-      current_on_chatroom.delete_at(current_on_chatroom.index(current_user))
+    if logged_in? && current_on_chatroom.include?(current_user.id)
+      current_on_chatroom.delete_at(current_on_chatroom.index(current_user.id))
     end
     $cache.set('current_on_chatroom', current_on_chatroom.to_json)
   end
 
   def add_to_chatroom
     current_on_chatroom = JSON.parse($cache.get('current_on_chatroom'))
-    if logged_in? && current_on_chatroom.include?(current_user) == false
-      current_on_chatroom.push(current_user)
+    if logged_in? && current_on_chatroom.include?(current_user.id) == false
+      current_on_chatroom.push(current_user.id)
     end
     $cache.set('current_on_chatroom', current_on_chatroom.to_json)
   end
 
   def read_from_chatroom
     current_on_chatroom = JSON.parse($cache.get('current_on_chatroom'))
-    return current_on_chatroom
+    all_online_chatroom_users = []
+    current_on_chatroom.each do |id_num|
+      all_online_chatroom_users.push(User.find(id_num))
+    end
+    return all_online_chatroom_users
   end
 
 end
